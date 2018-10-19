b3b7628ea478d0623bc7d1d4001221667ce4ea4d
Collin Bradley <herescollin@gmail.com>
2018-10-19 20:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function takeANumber(katzDeliLine,newPerson){
   katzDeliLine.push(newPerson);
-  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line`
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line`;
 }
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
-    return 
+    return "There is nobody waiting to be served!";
+  } else {
+    var number1 = katzDeliLine[0];
+    katzDeliLine.shift()
+    return number1
   }
 }
