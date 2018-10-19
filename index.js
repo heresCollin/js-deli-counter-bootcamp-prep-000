55bc8bc0db423ca0911e8ec20683e4a1cfe2d156
Collin Bradley <herescollin@gmail.com>
2018-10-19 20:45:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine,newPerson){
   katzDeliLine.push(newPerson);
-  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line`;
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine){
